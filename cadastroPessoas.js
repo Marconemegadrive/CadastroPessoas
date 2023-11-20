b40ab8b5c596f6bfaf29ee30e6591e258d9a3476
Marconemegadrive <116309473+Marconemegadrive@users.noreply.github.com>
2023-11-20 18:55:40 -0300

    cadastropessoas commit

---
+++ cadastroPessoas.js
@@ -0,0 +1,11 @@
+// Definindo variáveis para o cadastro de pessoa
+var nome = "João Silva";  // String
+var salario = 35000.50;   // Number
+var idade = 25;           // Number
+var possuiDiploma = true; // Boolean
+
+// Exibindo os valores no console
+console.log("Nome: " + nome);
+console.log("Salário: " + salario);
+console.log("Idade: " + idade);
+console.log("Possui diploma? " + possuiDiploma);
